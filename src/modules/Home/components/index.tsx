b4e8bebc0a23ece69a6ff7e872af5500c77c52ad
ekiolivier@gmail.com <ekiolivier@gmail.com>
2025-01-24 10:25:57 -0300

    add lowercase

--- src/modules/Home/components/index.tsx
+++ src/modules/Home/components/index.tsx
@@ -25,7 +25,7 @@ const HomePage = () => {
   const { recentSearches } = useWeather();
 
   const handleChipSearch = (city: string) => {
-    handleSearch(city);
+    handleSearch(city.toLowerCase());
   };
 
   return (
