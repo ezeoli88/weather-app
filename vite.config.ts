f0bfcf71e06f914c9db756daa0008562a5fa3798
ekiolivier@gmail.com <ekiolivier@gmail.com>
2025-01-31 11:20:31 -0300

    agrego react scan para medir performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
+import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom', // Establece jsdom como entorno de pruebas
-    globals: true,        // Habilita las funciones globales de Jest como `expect`
-    setupFiles: './src/setupTests.ts', // Archivo de configuración para las pruebas
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    plugins: [react(), MillionLint.vite()],
+    test: {
+        environment: 'jsdom', // Establece jsdom como entorno de pruebas
+        globals: true, // Habilita las funciones globales de Jest como `expect`
+        setupFiles: './src/setupTests.ts', // Archivo de configuración para las pruebas
+    },
+    optimizeDeps: {
+        exclude: ['lucide-react'],
+    },
 });
